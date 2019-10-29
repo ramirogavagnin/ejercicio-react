b914da9bd19c83a7394b06d2d4280a648a19b8d6
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2019-10-29 00:38:02 -0300

    money in row

--- src/components/row/index.jsx
+++ src/components/row/index.jsx
@@ -3,21 +3,30 @@ import Text from "../styled-text";
 import CustomSlider from "../slider";
 import "./row.css";
 
-const Row = ({ title, value, min, max, onChange }) => {
+const Row = ({ title, value, min, max, onChange, money }) => {
   return (
     <div className="height-50-p">
       <div className="space-between">
         <Text className="regular">{title}</Text>
         <div className="border padding-horizontal-4">
-          <Text className="bold">{value}</Text>
+          <Text className="bold">
+            {money && "$"}
+            {value}
+          </Text>
         </div>
       </div>
       <div className="slider-container">
         <CustomSlider value={value} min={min} max={max} onChange={onChange} />
       </div>
       <div className="space-between">
-        <Text className="thin">{min}</Text>
-        <Text className="thin">{max}</Text>
+        <Text className="thin">
+          {money && "$"}
+          {min}
+        </Text>
+        <Text className="thin">
+          {money && "$"}
+          {max}
+        </Text>
       </div>
     </div>
   );
